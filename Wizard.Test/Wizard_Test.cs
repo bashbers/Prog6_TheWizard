7b62325936d860bbdd768d535217b8ce5ea2e970
Stino <Linksonder@gmail.com>
2015-11-08 18:27:08 +0100

    Start project

---
+++ Wizard.Test/Wizard_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wizard.Test
+{
+    //[TestClass]
+    //public class Wizard_Test
+    //{
+    //    [TestMethod]
+    //    public void Foramisforameur_goed()
+    //    {
+    //        //1. Arrange
+    //        //2. Act
+    //        //3. Assert
+    //    }
+
+    //    [TestMethod]
+    //    public void Foramisforameur_fout()
+    //    {
+    //        //1. Arrange
+    //        //2. Act
+    //        //3. Assert
+    //    }
+    //}
+}
